ab2b61a2eeae8f9577bfa46696866883346781a4
Henry Aguilar <henrywaguilar505@gmail.com>
2025-10-20 11:22:45 -0600

    add public routes

--- routes/api.php
+++ routes/api.php
@@ -20,18 +20,21 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//rutas publicas
 Route::post('/login', [Authcontroller::class, 'login'])->name('login');
+  //mostrar todos los productos
+Route::get('products', [ProductController::class, 'index']);
+    //filtrar productos por categoria
+Route::get('products/category/{category_id}', [ProductController::class, 'filterByCategory']);
 
 
 //rutas protegidas con sanctum
 Route::middleware('auth:sanctum')->group(function () {
    
-    //filtrar productos por categoria
-    Route::get('products/category/{category_id}', [ProductController::class, 'filterByCategory']);
+
     //almacenar productos
     Route::post('products', [ProductController::class, 'store']);
-     //mostrar todos los productos
-    Route::get('products', [ProductController::class, 'index']);
+   
     //eliminar producto
     Route::delete('products/{id}', [ProductController::class, 'destroy']);
     //actualizar producto
